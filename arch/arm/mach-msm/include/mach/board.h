02436c71b0c6e90a5d2814889328481897c78d37
Stephen Boyd <sboyd@codeaurora.org>
2011-02-15 17:49:15 -0800

    msm: clock: Always use an array to iterate over clocks
    If the array of clk_lookups contains aliases for the same struct
    clk, msm_clock_init() will add the clock to the clocks list
    twice.  This would cause list corruption so let's just remove the
    clocks list and any associated code and iterate over the array
    instead.
    
    Change-Id: I0d97330f63c89996c26debf40e1f070b351822bf
    Signed-off-by: Stephen Boyd <sboyd@codeaurora.org>

--- arch/arm/mach-msm/include/mach/board.h
+++ arch/arm/mach-msm/include/mach/board.h
@@ -29,7 +29,7 @@ struct clk_lookup;
 void __init msm_add_devices(void);
 void __init msm_init_irq(void);
 void __init msm_init_gpio(void);
-void __init msm_clock_init(struct clk_lookup *clock_tbl, unsigned num_clocks);
+void __init msm_clock_init(struct clk_lookup *clock_tbl, size_t num_clocks);
 int __init msm_add_sdcc(unsigned int controller,
 			struct msm_mmc_platform_data *plat,
 			unsigned int stat_irq, unsigned long stat_irq_flags);
