RonGokhale/codeaurora_kernel

msm: clock: Add EBI1 voter clocks for ADM on SoCs without them
The ADM driver wishes to vote for EBI1 clock rates during ADM
transfers.  Add voter clocks for this purpose.

On msm8960, this also requires the creation of an EBI voter clock
for the msm_bus driver, since it was previously using the clock
directly.

Change-Id: I605cc910ffd73ef5d00af6b5ebf062ec5277279f
Signed-off-by: Matt Wagantall <mattw@codeaurora.org>

arch/arm/mach-msm/clock-7x30.c
@@ -2366,6 +2366,7 @@ static DEFINE_CLK_VOTER(ebi_mddi_clk, &ebi1_fixed_clk.c);
 static DEFINE_CLK_VOTER(ebi_tv_clk, &ebi1_fixed_clk.c);
 static DEFINE_CLK_VOTER(ebi_vcd_clk, &ebi1_fixed_clk.c);
 static DEFINE_CLK_VOTER(ebi_vfe_clk, &ebi1_fixed_clk.c);
+static DEFINE_CLK_VOTER(ebi_adm_clk, &ebi1_fixed_clk.c);
 
 /*
  * SoC-specific functions required by clock-local driver
@@ -2775,6 +2776,7 @@ static struct clk_local_ownership {
 	{ CLK_LOOKUP("ebi1_tv_clk",	ebi_tv_clk.c,	NULL) },
 	{ CLK_LOOKUP("ebi1_vcd_clk",	ebi_vcd_clk.c,	NULL) },
 	{ CLK_LOOKUP("ebi1_vfe_clk",	ebi_vfe_clk.c,	NULL) },
+	{ CLK_LOOKUP("ebi1_clk",	ebi_adm_clk.c,	"msm_dmov") },
 
 	/*
 	 * This is a many-to-one mapping because we don't know how the remote

kernelorg-mirror/linux_kernel_git_davidb_linux-msm

arch/arm/mach-msm/clock-7x30.c
@@ -2373,6 +2373,7 @@ static DEFINE_CLK_VOTER(ebi_mddi_clk, &pbus_clk.c);
 static DEFINE_CLK_VOTER(ebi_tv_clk, &pbus_clk.c);
 static DEFINE_CLK_VOTER(ebi_vcd_clk, &pbus_clk.c);
 static DEFINE_CLK_VOTER(ebi_vfe_clk, &pbus_clk.c);
+static DEFINE_CLK_VOTER(ebi_adm_clk, &pbus_clk.c);
 
 /*
  * SoC-specific functions required by clock-local driver
@@ -2757,6 +2758,7 @@ static struct clk_local_ownership {
 	{ CLK_LOOKUP("ebi1_tv_clk",	ebi_tv_clk.c,	NULL) },
 	{ CLK_LOOKUP("ebi1_vcd_clk",	ebi_vcd_clk.c,	NULL) },
 	{ CLK_LOOKUP("ebi1_vfe_clk",	ebi_vfe_clk.c,	NULL) },
+	{ CLK_LOOKUP("ebi1_clk",	ebi_adm_clk.c,	"msm_dmov") },
 
 	/*
 	 * This is a many-to-one mapping because we don't know how the remote